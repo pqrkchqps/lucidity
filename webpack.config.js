1817f46f48663b0ab044b0adebc84562d3c12a75
John <johnacreps@gmail.com>
2022-12-08 14:11:27 -0600

    Made webpack and babel work to successfully run build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = (env) => {
   const isProduction = env === 'production'
-  const CSSExtract = new ExtractTextPlugin('styles.css')
 
   return {
     entry: ['babel-polyfill', './src/app.js'],
@@ -16,28 +14,13 @@ module.exports = (env) => {
         loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
-      }, {
-        test: /\.s?css$/,
-        use: CSSExtract.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ]
-        })
+      },
+      {
+    	test: /\.(sass|scss|less|css)$/,
+    	use: ['style-loader', 'css-loader', 'sass-loader']
       }]
     },
     plugins: [
-      CSSExtract
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
